1bc435fb6c8542631a9fd18d4153bb2f2431285b
edgetopher <edgetopher@gmail.com>
2019-07-02 11:12:05 -0400

    init

---
+++ src/main/java/com/tutorial/Game/ID.java
@@ -0,0 +1,7 @@
+package com.tutorial.Game;
+
+public enum ID {
+
+    Player(),
+    Enemy();
+}
---
+++ src/main/java/com/tutorial/Game/Player.java
@@ -0,0 +1,20 @@
+package com.tutorial.Game;
+
+import java.awt.*;
+
+public class Player extends GameObject {
+
+    public Player(int x, int y, ID id) {
+        super(x, y, id);
+    }
+
+    public void tick() {
+
+    }
+
+    public void render(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.fillRect(x, y, 32,32);
+
+    }
+}
